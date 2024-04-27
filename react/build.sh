1e13f2a9cd5d125edc96430cef4b466e345cc9dd
Devecor <devecor@outlook.com>
2024-04-27 23:08:50 +0800

    fix test

--- react/build.sh
+++ react/build.sh
@@ -3,6 +3,6 @@
 cwd=$PWD
 for i in src/components/*; do
   yarn vite build $i -c vite.config.mts
-  cd $cwd/$i && ../../../../node_modules/.bin/tsc
+  cd $cwd/$i && ../../../../node_modules/.bin/tsc --project tsconfig.build.json
   cd $cwd
 done
--- react/webpack.config.js
+++ react/webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
           loader: 'babel-loader',
           options: {
             presets: [
-              '@babel/react',
+              ['@babel/react', {runtime: 'automatic'}],
               '@babel/preset-typescript'
             ]
           }
